112702e1912f9073615f74b3ac28b5ad66bbf3d1
Rey-86 <reygranhacristian@gmail.com>
2023-02-14 18:44:53 +0100

    Readme y login

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { getFirebaseConfig } from "./firebase-config";
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getFirebaseConfig } from './firebase-config'
 
-const firebaseAppConfig = getFirebaseConfig();
+const firebaseAppConfig = getFirebaseConfig()
 
 // Initialize Firebase
-const app = initializeApp(firebaseAppConfig);
-const auth = getAuth();
-let email="";
-let password="";
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
+const app = initializeApp(firebaseAppConfig)
+const auth = getAuth()
+
+window.onload = () => {
+
+  // Seleccionar elementos de la página
+  let btnLogin = document.getElementById('btnLogin')
+  let inputEmail = document.getElementById('email')
+  let inputPassword = document.getElementById('password')
+
+  // Asociamos eventos
+  btnLogin.addEventListener('click', () => {
+    let email = inputEmail.value;
+    let password = inputPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user
+        // ...
+      })
+      .catch(error => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log(errorMessage)
+      })
   })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
-  });
+}
