30f59f7ce0de7f3216e7ba8e8596857d8d2a89a7
Rey-86 <reygranhacristian@gmail.com>
2023-02-15 18:18:46 +0100

    guardar cita y añadir página clínica

--- src/index.js
+++ src/index.js
@@ -1,51 +1,54 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithEmailAndPassword,onAuthStateChanged,
+import { initializeApp } from "firebase/app";
+import {getAuth, signInWithEmailAndPassword, onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
-  signOut} from 'firebase/auth'
-import { getFirebaseConfig } from './firebase-config'
+  signOut
+} from "firebase/auth";
+import {getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,} from 'firebase/firestore';
+import { getFirebaseConfig } from "./firebase-config";
 
-const firebaseAppConfig = getFirebaseConfig()
+const firebaseAppConfig = getFirebaseConfig();
 
 // Initialize Firebase
-const app = initializeApp(firebaseAppConfig)
-const auth = getAuth()
+const app = initializeApp(firebaseAppConfig);
+const auth = getAuth();
 
-onAuthStateChanged(auth, (user)=>{
-  if(user){
-    //Login
-    //alert(auth.currentUser.displayName)
-  }else{
-    alert("cerrando sesión");
-    //logout
-  }
-});
 
 window.onload = () => {
-
-  // Seleccionar elementos de la página
-  let btnLogin = document.getElementById('btnLogin');
-  let btnGoogle=document.getElementById("btnGoogle");
-  let btnCerrar=document.getElementById("btnCerrar");
-  let inputEmail = document.getElementById('email');
-  let inputPassword = document.getElementById('password');
+  //Seleccionamos elementos de la página
+  let btnLogin = document.getElementById("btnLogin");
+  let btnGoogle = document.getElementById("btnGoogle");
+  let btnCerrar = document.getElementById("btnCerrar");
+  let inputEmail = document.getElementById("email");
+  let inputPassword = document.getElementById("password");
   let sectionLogin = document.getElementById("section_login");
   let sectionMain = document.getElementById("section_main");
-
-  // Asociamos eventos
-  btnLogin.addEventListener('click', () => {
+  let btnCrearCita=document.getElementById("btnCrearCita");
+  let inputNombre=document.getElementById("nombre");
+  let inputApellido=document.getElementById("apellido");
+  let inputTelefono=document.getElementById("telefono");
+  let inputHora=document.getElementById("hora");
+  let inputFecha=document.getElementById("fecha");
+  let inputSintomas=document.getElementById("sintomas");
+  //Asociamos eventos
+  btnLogin.addEventListener("click", () => {
     let email = inputEmail.value;
     let password = inputPassword.value;
     signInWithEmailAndPassword(auth, email, password)
-      .then(userCredential => {
-        // Signed in
-        const user = userCredential.user
+      .then((userCredential) => {
+        // Signed in 
+
+        const user = userCredential.user;
+        console.log(user);
         // ...
       })
-      .catch(error => {
-        const errorCode = error.code
-        const errorMessage = error.message
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
         alert(errorMessage);
       });
   })
@@ -54,25 +57,55 @@ window.onload = () => {
     signIn();
   })
 
-  btnCerrar.addEventListener("click",()=>{
+  btnCerrar.addEventListener("click", () => {
     signOut(auth);
   })
+
+  btnCrearCita.addEventListener("click",()=>{
+    let cita={
+      nombre:inputNombre.value,
+      apellido:inputApellido.value,
+      telefono:inputTelefono.value,
+      fecha:inputFecha.value,
+      hora:inputHora.value,
+      sintomas:inputSintomas.value
+    }
+    crearCita(cita);
+  })
+
+
   onAuthStateChanged(auth, (user) => {
     if (user) {
       //Login
-
       sectionLogin.hidden = true;
-      sectionMain.hidden = false;
+      sectionMain.hidden=false;
+      let username=user.displayName!=null?user.displayName:user.email;
+      document.getElementById("username").innerText=username;
+    
     } else {
 
       sectionLogin.hidden = false;
-      sectionMain.hidden = true;
+      sectionMain.hidden=true;
       //logout
     }
   });
 }
+
 async function signIn() {
   // Sign in Firebase using popup auth and Google as the identity provider.
   var provider = new GoogleAuthProvider();
   await signInWithPopup(getAuth(), provider);
+}
+
+// Saves a new Object to Cloud Firestore.
+async function crearCita(cita) {
+  // Add a new object entry to the Firebase database.
+  cita.timestamp=serverTimestamp();
+  cita.user=getAuth().currentUser.email;
+  try {
+    await addDoc(collection(getFirestore(), 'citas'), cita);
+  }
+  catch(error) {
+    alert('Error writing new cita to Firebase Database', error);
+  }
 }
