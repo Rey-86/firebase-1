5dc4b48d111e8f07acf03e5d345f1e633abfe85e
Rey-86 <reygranhacristian@gmail.com>
2023-02-16 18:42:46 +0100

    edit document

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
   signOut
 } from "firebase/auth";
 import {
+  firestore,
   getFirestore,
   collection,
   addDoc,
@@ -16,7 +17,10 @@ import {
   orderBy,
   limit,
   onSnapshot,
-  deleteDoc
+  deleteDoc,
+  updateDoc,
+  setDoc,
+  getDoc
 } from 'firebase/firestore';
 import { getFirebaseConfig } from "./firebase-config";
 
@@ -25,7 +29,7 @@ const firebaseAppConfig = getFirebaseConfig();
 // Initialize Firebase
 const app = initializeApp(firebaseAppConfig);
 const auth = getAuth();
-
+var docRef = null;
 
 
 
@@ -40,6 +44,7 @@ window.onload = () => {
   let sectionLogin = document.getElementById("section_login");
   let sectionMain = document.getElementById("section_main");
   let btnCrearCita = document.getElementById("btnCrearCita");
+  let btnEditarCita = document.getElementsByClassName("btnEditarCita")[0];
   let inputNombre = document.getElementById("nombre");
   let inputApellido = document.getElementById("apellido");
   let inputTelefono = document.getElementById("telefono");
@@ -84,6 +89,23 @@ window.onload = () => {
     }
     crearCita(cita);
   })
+  btnEditarCita.addEventListener("click", () => {
+    let cita = {
+      nombre: inputNombre.value,
+      apellido: inputApellido.value,
+      fecha: inputFecha.value,
+      hora: inputHora.value,
+      sintomas: inputSintomas.value,
+      telefono: inputTelefono.value
+    }
+    updateDoc(docRef, cita)
+      .then(() => {
+        borrarCampos();        
+       })
+      .catch((err) => { alert(err) })
+
+
+  })
 
 
   onAuthStateChanged(auth, (user) => {
@@ -108,6 +130,11 @@ window.onload = () => {
       const element = inputs[i];
       element.value = "";
     }
+    btnCrearCita.classList.remove("d-none");
+    btnCrearCita.classList.add("d-block");
+    btnEditarCita.id = "";
+    btnEditarCita.classList.remove("d-block");
+    btnEditarCita.classList.add("d-none");
   }
   async function signIn() {
     // Sign in Firebase using popup auth and Google as the identity provider.
@@ -132,25 +159,32 @@ window.onload = () => {
   function cargarCitas() {
     // Create the query to load the last 12 messages and listen for new ones.
     const recentCitasQuery = query(collection(getFirestore(), 'citas'), orderBy('timestamp', 'desc'), limit(8));
-    
+
     // Start listening to the query.
-    onSnapshot(recentCitasQuery, function(snapshot) {
-      let citas="";
-      snapshot.forEach(doc=>{
-        let cita=doc.data();
-        citas+=`<div id=${doc.id} class="cita col-5"><p>Paciente: ${cita.nombre} ${cita.apellido}</p>
+    onSnapshot(recentCitasQuery, function (snapshot) {
+      let citas = "";
+      snapshot.forEach(doc => {
+        let cita = doc.data();
+        citas += `<div id=${doc.id} class="cita col-5"><p>Paciente:${cita.nombre} ${cita.apellido}</p>
         <p>Fecha: ${cita.fecha} - ${cita.hora}</p>
         <p>Sintomas: ${cita.sintomas}</p>
         <i class="fa-solid fa-trash borrarCita"></i>
         <i class="fa-solid fa-pen-to-square editarCita"></i></div>`;
       })
-      
-      document.getElementById("citas").innerHTML=citas;
-      let btnsBorrar=document.getElementsByClassName("borrarCita");
+
+      document.getElementById("citas").innerHTML = citas;
+      let btnsBorrar = document.getElementsByClassName("borrarCita");
       for (let i = 0; i < btnsBorrar.length; i++) {
-        btnsBorrar[i].addEventListener("click",(e)=>{
-           let idDoc=e.currentTarget.parentElement.id;
-           borrarCita(idDoc);
+        btnsBorrar[i].addEventListener("click", (e) => {
+          let idDoc = e.currentTarget.parentElement.id;
+          borrarCita(idDoc);
+        })
+      }
+      let btnsEditar = document.getElementsByClassName("editarCita");
+      for (let i = 0; i < btnsEditar.length; i++) {
+        btnsEditar[i].addEventListener("click", (e) => {
+          let idDoc = e.currentTarget.parentElement.id;
+          editarCita(idDoc)
         })
 
       }
@@ -158,16 +192,34 @@ window.onload = () => {
   }
 
   function borrarCita(idDoc) {
-    let docRef=doc(getFirestore(),"citas",idDoc);
+    docRef = doc(getFirestore(), "citas", idDoc);
     deleteDoc(docRef)
-    .then(()=>{
-      
-    })
+      .then(() => {
+
+      })
       .catch((err) => {
-        alert(err);
-    })
+        alert(err)
+      })
   }
-  
+  async function editarCita(idDoc) {
+    docRef = doc(getFirestore(), "citas", idDoc);
+
+    let citaSnap = await getDoc(docRef);
+    inputNombre.value = citaSnap.data().nombre;
+    inputApellido.value = citaSnap.data().apellido;
+    inputHora.value = citaSnap.data().hora;
+    inputFecha.value = citaSnap.data().fecha;
+    inputTelefono.value = citaSnap.data().telefono;
+    inputSintomas.value = citaSnap.data().sintomas;
+    btnCrearCita.classList.remove("d-block");
+    btnCrearCita.classList.add("d-none");
+    btnEditarCita.id = "btn-" + citaSnap.id;
+    btnEditarCita.classList.remove("d-none");
+    btnEditarCita.classList.add("d-block");
+
+
+  }
 
   cargarCitas();
 }
+
